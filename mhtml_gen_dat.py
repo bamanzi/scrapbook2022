2015869ce3174046a3721a335c2c818a999aa3e0
Ba Manzi <bamanzi@gmail.com>
2022-03-12 15:50:25 +0800

    Update mhtml_gen_dat.py

--- mhtml_gen_dat.py
+++ mhtml_gen_dat.py
@@ -7,6 +7,7 @@ import sys
 import string
 import re
 from datetime import datetime
+import urllib.parse
 import email.header
 import shutil
 import pdb
@@ -21,9 +22,9 @@ icon	favicon.ico.bin
 source	%(url)s
 comment	
 lock	
-folder 
+folder	%(folder)s
 container	
-exported	2022-03-11T08:08:48.380Z
+exported	%(export_time)s
 """
 
 def parse_header(line):
@@ -34,23 +35,26 @@ def parse_header(line):
         title, encoding = titles[0]
         if encoding:
             try:
-                return 'title', title.decode(encoding)
+                return { 'title': title.decode(encoding) }
             except UnicodeDecodeError:
-                return 'title', None
+                return { 'title': None }
         else:
-            return 'title', title
+            return { 'title': title }
     elif line.startswith("Date: "):
         # e.g. Date: Sun, 3 Jan 2021 14:25:37 -0000
         s = line[6:]
-        d = datetime.strptime(s, "%a, %d %b %Y %H:%M:%S -0000")
-        ds = d.strftime("%Y%m%d%H%M%S")
-        return 'id', ds
+        dt = datetime.strptime(s, "%a, %d %b %Y %H:%M:%S -0000")
+        
+        id_str = dt.strftime("%Y%m%d%H%M%S")
+        # 2022-03-11T08:08:48.380Z
+        export_time = dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+        return  { 'id': id_str, 'export_time': export_time }
     elif line.startswith("Snapshot-Content-Location: "):
         start = len("Snapshot-Content-Location: ")
         url = line[start:]
-        return 'url', url
+        return { 'url': url }
     else:
-        return None, None
+        return {}
 
 HEADER_RE = "^[A-Z][A-Za-z-]: "
 def is_mime_header_line(line):
@@ -71,8 +75,8 @@ def parse_mhtml_headers(mhtml_filename):
         if is_mime_header_line(line):
             # current line is a MIME header
             header = ''.join(lines)
-            key, value = parse_header(header)
-            if key:
+            props = parse_header(header)
+            for key, value in props.items():
                 article[key] = value
             lines.clear()
                 
@@ -99,29 +103,60 @@ def gen_index_dat(mhtml_filename, article):
         return False
 
     with open(output_filename, 'w') as fo:
-        print("writing to index.dat...")
         fo.write(TEMPLATE % article)
         return True
-            
+
+
+def copy_index_html(basename, target_dir):
+    """ Copy `foobar.html` (converted by Mozilla Archive Format) to `foobar_files/index.html`.
+
+    And remove all occcurences of `foobar_files/` in target file (`foobar_files/index.html`).
+    """
+    source_file = basename + '.html'
+    
+    files_dir_q = urllib.parse.quote(basename + "_files")
+    # gotchar: '()' shouldn't be quoted
+    files_dir_q = files_dir_q.replace('%28', '(').replace('%29', ')').replace('%26', '&amp;')
+    
+    target_file = os.path.join(target_dir, 'index.html')
+    with open(source_file) as fin, open(target_file, 'w') as fout:
+        while True:
+            line = fin.readline()
+            if not line:
+                break        
+            line_t = line.replace(files_dir_q + "/", "")
+            fout.write(line_t)
+    shutil.move(source_file, os.path.join(target_dir, "index.html.bak"))    
+    print("  index.html.bak %d" % os.stat(target_file + '.bak').st_size)
+    print("  index.html     %d" % os.stat(target_file).st_size)
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Generate index.dat (for scrapbook) from MHTML files\n")
         print("Usage: %s foo.mhtml bar.mhtml...")
 
+    folder = os.path.split(os.getcwd())[1]
     for fn in sys.argv[1:]:
         basename, extname = os.path.splitext(fn)
+        files_dir = basename + "_files"
+        
         if extname.lower() != '.mhtml':
-            print("%s: skipped\n" % fn)
+            print(">>> %s: skipped (not mhtml file)\n" % fn)
+        elif not os.path.exists(files_dir):
+            print('>>> %s: skipped (..._files dir not exist)\n' % fn)
         else:
-            print("%s: processing..." % fn)
+            print(">>> %s" % fn)
             article = parse_mhtml_headers(fn)
+            article['folder'] = folder
+
+            print("  - generating index.dat")
             ok = gen_index_dat(fn, article)
 
             if True:
                 html_file = basename + '.html'
-                files_dir = basename + "_files"
                 #pdb.set_trace()
                 if os.path.isdir(files_dir) and os.path.isfile(html_file):
-                    shutil.move(html_file, os.path.join(files_dir, "index.html"))
+                    print("  - convert xxx.html to xxx_files/index.html")
+                    copy_index_html(basename, files_dir)
             print("done")
