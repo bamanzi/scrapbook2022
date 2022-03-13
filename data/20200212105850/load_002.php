9736e7451e50b29722764903f92ba50224e7e190
Ba Manzi <bamanzi@gmail.com>
2022-03-13 09:23:27 +0800

    delete some articles already in scrapbook2020

--- data/20200212105850/load_002.php
+++
@@ -1,9 +0,0 @@
-/* Code tidied up by ScrapBook */
-@media only screen and (min-width: 1596px) {
-}
-@media only screen and (min-width: 1084px) and (max-width: 1595px) {
-}
-@media only screen and (min-width: 1024px) and (max-width: 1083px) {
-}
-@media only screen and (max-width: 1023px) {
-}
